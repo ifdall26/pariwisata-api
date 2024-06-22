5fe64c8bc1a6480e6bed9a889450cb7c62553239
ifdall26 <lisyukri.ifdal@gmail.com>
2024-06-22 15:02:59 +0700

    menghapus password

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('pariwisata', 'root', 'password', {
+const sequelize = new Sequelize('pariwisata', 'root', '', {
   host: 'localhost',
   dialect: 'mysql'
 });
