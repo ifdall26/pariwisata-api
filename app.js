ffe977ff4bb79cf3f5d29a6e3adb694ab46b44bc
ifdall26 <lisyukri.ifdal@gmail.com>
2024-07-21 02:55:20 +0700

    menghapus cors

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');
-const morgan = require('morgan');
 const sequelize = require('./config/database');
 const destinasiRoutes = require('./routes/destinasi');
 const authRoutes = require('./routes/auth');
